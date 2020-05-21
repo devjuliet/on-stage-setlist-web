678ad4978706c24e457d40970d5a047680138627
Flor <legarda.flor@gmail.com>
2020-05-20 23:03:33 -0600

    Elaboración vista de ajustes y catalogos

--- src/app/views/dashboard/manager/catalog/catalog.component.ts
+++ src/app/views/dashboard/manager/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilitiesService } from 'src/app/services/utilities/utilities.service';
 
 @Component({
   selector: 'app-catalog',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public utilitiesService : UtilitiesService) { }
 
   ngOnInit(): void {
   }
--- src/app/views/dashboard/manager/settings/settings.component.ts
+++ src/app/views/dashboard/manager/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilitiesService } from 'src/app/services/utilities/utilities.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public utilitiesService : UtilitiesService) { }
 
   ngOnInit(): void {
   }
